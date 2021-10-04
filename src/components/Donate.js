fe07fd96d9ec6058f47108cfb8602ec1aa243a9f
Laura Medina <lcmedina390@gmail.com>
2021-10-04 12:16:19 -0500

    donation page styled

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -3,33 +3,50 @@ import ReactDOM from "react-dom"
 import TextField from '@mui/material/TextField';
 import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { useHistory, Redirect } from 'react-router-dom'
+import InputAdornment from '@mui/material/InputAdornment';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
 
 function Donate() {
     const history = useHistory();
   return (
     <PayPalScriptProvider options={{ "client-id": "test" }}>
-    <TextField type="number" id="amount"></TextField>
-   <PayPalButtons
-       style={{ layout: "horizontal" }}
-       createOrder={(data, actions) => {
-           return actions.order.create({
-               purchase_units: [
-                   {
-                       amount: {
-                           value: document.getElementById('amount').value,
-                       },
-                   },
-               ],
-           });
-       }}
-       onApprove={(data, actions) => {
-           return actions.order.capture().then((details) => {
-              alert("Thanks for donating " + details.payer.name.given_name + "!")
-           })
-       }}
-   />
-</PayPalScriptProvider>
+            <div className="donation">
+            <h1 style={{paddingBottom: '25px'}}>Love our Site?</h1>
+            <h3 style={{paddingBottom: '25px'}}>Please consider making a donation so that we can continue providing out service to you!</h3>
+            <TextField 
+            type="number" 
+            id="amount" 
+            variant="filled"
+            InputProps={{
+                startAdornment: (
+                <InputAdornment position="start">
+                    <MonetizationOnIcon/>
+                </InputAdornment>
+                )
+            }}
+            />
+            </div>
+            <PayPalButtons
+            style={{ color: "blue", shape: "pill", label: "pay", height: 40 }}
+            createOrder={(data, actions) => {
+                return actions.order.create({
+                    purchase_units: [
+                        {
+                            amount: {
+                                value: document.getElementById('amount').value,
+                            },
+                        },
+                    ],
+                });
+            }}
+            onApprove={(data, actions) => {
+                return actions.order.capture().then((details) => {
+                    alert("Thanks for donating " + details.payer.name.given_name + "!")
+                })
+            }}
+        />
+    </PayPalScriptProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,7 @@ const Header = () => {
        }
     const buttons = [
         <Button key="one"><Link to="/donate" className="text-link">Donate</Link></Button>,
-        <Button key="two"><Link to="/contact" className="text-link">Contact</Link></Button>,
-        <Button key="three" onClick={toggleTheme} style={{color: "white"}}>{darkTheme ? <Brightness7Icon/> : <Brightness4Icon/>}</Button>,
+        <Button key="three" onClick={toggleTheme}>{darkTheme ? <Brightness7Icon/> : <Brightness4Icon/>}</Button>,
       ];
 
     return (
@@ -26,7 +25,7 @@ const Header = () => {
                 <div className="navbar">
                 <Link to="/" className="text-link"><h1><NoteAltOutlinedIcon/> MyKeeper</h1></Link>
                 <div className="links">
-                <ButtonGroup size="large" aria-label="large button group">
+                <ButtonGroup color="inherit" size="large" aria-label="large button group">
                 {buttons}
                 </ButtonGroup>
                 </div>
