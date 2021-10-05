6bd898134b8a728672ea47658a2d43207899bacd
Laura Medina <lcmedina390@gmail.com>
2021-10-05 14:38:39 -0500

    fixed uuid bug

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom";
 import PriorityHighOutlinedIcon from '@mui/icons-material/PriorityHighOutlined';
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 import { useTheme } from "./ThemeContext";
-import { v4 as uuidv4 } from "uuid";
 
 const NotesList = ({ notes }) => {
-    const uuid = uuidv4();
     const darkTheme = useTheme()
     const themeStyles= {
             color: darkTheme ? '#333' : '#f5ba13',
@@ -14,7 +12,7 @@ const NotesList = ({ notes }) => {
     return (
         <div>
         {notes.map((eachNote) => {
-            return <div className="note" key={uuid}>
+            return <div className="note" key={eachNote.id}>
                 <h1 style={{color: darkTheme ? '#f5ba13' : '#333'}}>{eachNote.title}</h1>
                 <div><p>{eachNote.content}</p></div>
                 <button style={themeStyles}><PriorityHighOutlinedIcon/></button>
